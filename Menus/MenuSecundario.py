542062faa1384e88fa7e61c1b777b4574288f580
AuraCamilaPicoAraque <auroraprogamer@gmail.com>
2025-02-08 10:03:35 -0500

    ProyectoParte1

---
+++ Menus/MenuSecundario.py
@@ -0,0 +1,25 @@
+from menuPrincipal import *
+
+
+def MenuTrainer ():
+        print ("#####################################")
+        print("##¿Que quieres hacer como Trainer ? ###")
+        print ("#####################################")
+        print("1- Ver grupo")
+        print("2- Asignar notas")
+    
+def MenuCamper ():
+        print ("#####################################")
+        print("##¿Que quieres hacer como Camper ? ##")
+        print ("#####################################")
+        print("1- ver Notas")
+        print("2- ver cursos")
+
+
+def MenuCoordinador ():
+        print ("#####################################")
+        print("##¿Que quieres hacer como Coordinador ? ##")
+        print ("##########################################")
+        print("2- Ver grupos")
+        print("2- Ingresar nuevo camper")
+        print("3- Ingresar nuevo trainer")
---
+++ Menus/menuPrincipal.py
@@ -0,0 +1,48 @@
+import json
+from MenuSecundario import *
+
+##  Abrir
+def abrirCamperJSON ():
+    Abrir = {}
+    with open("../bbdd/camper.json" , "r") as openFile :
+        Abrir = json.load (openFile)
+    return Abrir
+    
+
+## Guardar
+
+def guardarCamperJSON (lool):
+    with open ("../bbdd/Coordinador.json" , "w") as outFile :
+        json.dump (lool , outFile)
+
+
+
+menu = {}
+bo=True
+while bo==True:
+      
+    def MenuDelPrincipio () :
+        print ("##########################################")
+        print("\n ----- Bienvenidos al CampusLand --------")
+        print ("##########################################")
+        print("¿Quien eres?")
+        print("1- Trainer")
+        print("2- Campers")
+        print("3- Coordinador ")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+                MenuTrainer()
+        elif opcion == "2":
+                MenuCamper()
+        elif opcion == "3":
+                MenuCoordinador()
+        elif opcion == "4":
+            print(" Saliendo del sistema...")
+            break
+        else:
+            print(" pción no válida, intente de nuevo.")
+
+    
+
